9796f0f8fac0edc721e4e8cc731454ceb0246ab9
Kurokid0206 <hungtyvuive@gmail.com>
2022-04-07 23:04:29 +0700

    add privileges, create/drop user/role procs

--- qlCSYT/Frm_Priviledges.Designer.cs
+++ qlCSYT/Frm_Priviledges.Designer.cs
@@ -36,7 +36,7 @@ namespace qlCSYT
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(557, 142);
+            this.dataGridView1.Location = new System.Drawing.Point(197, 102);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(240, 150);
             this.dataGridView1.TabIndex = 0;
