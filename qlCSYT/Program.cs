cded98abee4161943a56d2eadf72b66f480f8468
Kurokid0206 <hungtyvuive@gmail.com>
2022-04-07 15:14:20 +0700

    re arrange

--- qlCSYT/Program.cs
+++ qlCSYT/Program.cs
@@ -12,6 +12,7 @@ using Oracle.DataAccess.Types;
 using System.Windows.Forms;
 using qlCSYT;
 
+
 namespace CsOracleTutorial
 {
 
@@ -28,9 +29,6 @@ namespace CsOracleTutorial
                 conn.Open();
                 Console.WriteLine(conn.ConnectionString);
                 Console.WriteLine("Successful Connection");
-                
-
-                QueryEmployee(conn);
                 //ShowTable(conn);
             }
             catch (Exception e)
@@ -48,63 +46,6 @@ namespace CsOracleTutorial
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frm_Admin());
-        }
-
-        private static void QueryEmployee(OracleConnection conn)
-        {
-            string sql = "Select table_name from user_tables";
-
-            // Tạo một đối tượng Command.
-            OracleCommand cmd = new OracleCommand(sql, conn);
-            //cmd.Connection = conn;
-            //cmd.CommandText = "Select EMPNO,ENAME from C##demo.EMP_19120674";
-
-
-            using (DbDataReader reader = cmd.ExecuteReader())
-            {
-                if (reader.HasRows)
-                {
-                    Console.WriteLine("tbl name");
-                    while (reader.Read())
-                    {
-                        // Chỉ số (index) của cột Emp_ID trong câu lệnh SQL.
-                        //int empIdIndex = reader.GetOrdinal("EMPNO"); // 0
-
-                        string tbl_Name = reader.GetString(0);
-                        //int empNameIndex = reader.GetOrdinal("EName");// 2
-                        //string empName = reader.GetString(empNameIndex);
-
-
-                        //Console.WriteLine("empIdIndex:" + empIdIndex);
-                        Console.WriteLine(tbl_Name);
-                        //Console.WriteLine("EmpName:" + empName);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("failed");
-                }
-            }
-
-        }
-        private static void ShowTable(OracleConnection conn)
-        {
-            OracleCommand cmd = new OracleCommand("ShowTables", conn);
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.Add("vCHASSIS_RESULT", OracleDbType.RefCursor, ParameterDirection.InputOutput);
-
-
-            OracleDataAdapter da = new OracleDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            da.Fill(dt);
-
-            foreach (DataRow dataRow in dt.Rows)
-            {
-                foreach (var item in dataRow.ItemArray)
-                {
-                    Console.WriteLine(item);
-                }
-            }
         }
     }
 
---
+++ qlCSYT/functions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorial.SqlConn;
+using System.Data;
+using System.Data.Common;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+
+namespace qlCSYT
+{
+    class functions
+    {
+        private static void QueryEmployee(OracleConnection conn)
+        {
+            string sql = "Select table_name from user_tables";
+
+            // Tạo một đối tượng Command.
+            OracleCommand cmd = new OracleCommand(sql, conn);
+            //cmd.Connection = conn;
+            //cmd.CommandText = "Select EMPNO,ENAME from C##demo.EMP_19120674";
+
+
+            using (DbDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    Console.WriteLine("tbl name");
+                    while (reader.Read())
+                    {
+                        // Chỉ số (index) của cột Emp_ID trong câu lệnh SQL.
+                        //int empIdIndex = reader.GetOrdinal("EMPNO"); // 0
+
+                        string tbl_Name = reader.GetString(0);
+                        //int empNameIndex = reader.GetOrdinal("EName");// 2
+                        //string empName = reader.GetString(empNameIndex);
+
+
+                        //Console.WriteLine("empIdIndex:" + empIdIndex);
+                        Console.WriteLine(tbl_Name);
+                        //Console.WriteLine("EmpName:" + empName);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("failed");
+                }
+            }
+
+        }
+        private static void ShowTable(OracleConnection conn)
+        {
+            OracleCommand cmd = new OracleCommand("ShowTables", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("vCHASSIS_RESULT", OracleDbType.RefCursor, ParameterDirection.InputOutput);
+
+
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            foreach (DataRow dataRow in dt.Rows)
+            {
+                foreach (var item in dataRow.ItemArray)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
