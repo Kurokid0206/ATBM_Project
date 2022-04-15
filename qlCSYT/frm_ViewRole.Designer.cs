a7a07360878e710e1d3452d771bc9a30ffe7b32e
Mean-Sun <tranminhsonyi@gmail.com>
2022-04-15 15:54:31 +0700

    add form view role

--- qlCSYT/frm_ViewRole.Designer.cs
+++ qlCSYT/frm_ViewRole.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace qlCSYT
 {
-    partial class Frm_Edit_Roles
+    partial class frm_ViewRole
     {
         /// <summary>
         /// Required designer variable.
@@ -29,35 +29,37 @@ namespace qlCSYT
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_test = new System.Windows.Forms.Button();
+            this.GridView_ShowRole = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView_ShowRole)).BeginInit();
             this.SuspendLayout();
             // 
-            // btn_test
+            // GridView_ShowRole
             // 
-            this.btn_test.Location = new System.Drawing.Point(350, 130);
-            this.btn_test.Name = "btn_test";
-            this.btn_test.Size = new System.Drawing.Size(75, 23);
-            this.btn_test.TabIndex = 0;
-            this.btn_test.Text = "Tets";
-            this.btn_test.UseVisualStyleBackColor = true;
-            this.btn_test.Click += new System.EventHandler(this.btn_test_Click);
+            this.GridView_ShowRole.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridView_ShowRole.Location = new System.Drawing.Point(83, 36);
+            this.GridView_ShowRole.Name = "GridView_ShowRole";
+            this.GridView_ShowRole.RowHeadersWidth = 51;
+            this.GridView_ShowRole.RowTemplate.Height = 24;
+            this.GridView_ShowRole.Size = new System.Drawing.Size(657, 373);
+            this.GridView_ShowRole.TabIndex = 0;
             // 
-            // Frm_Edit_Roles
+            // frm_ViewRole
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(833, 427);
-            this.Controls.Add(this.btn_test);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.Name = "Frm_Edit_Roles";
+            this.ClientSize = new System.Drawing.Size(1111, 526);
+            this.Controls.Add(this.GridView_ShowRole);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "frm_ViewRole";
             this.Text = "Phân quyền User/Role";
             this.Load += new System.EventHandler(this.Frm_Edit_Roles_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GridView_ShowRole)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btn_test;
+        private System.Windows.Forms.DataGridView GridView_ShowRole;
     }
 }
