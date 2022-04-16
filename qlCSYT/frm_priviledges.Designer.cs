fe015d6a02133e20170cb7f1aebe64befc3b3978
Mean-Sun <tranminhsonyi@gmail.com>
2022-04-16 07:54:47 +0700

    Cấp quyền cho user.

--- qlCSYT/frm_priviledges.Designer.cs
+++ qlCSYT/frm_priviledges.Designer.cs
@@ -41,6 +41,7 @@ namespace qlCSYT
             this.lb_User = new System.Windows.Forms.Label();
             this.lb_Role = new System.Windows.Forms.Label();
             this.lb_Table = new System.Windows.Forms.Label();
+            this.GrantOpt_btn = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.gv_tables)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,7 +50,7 @@ namespace qlCSYT
             this.gv_tables.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.gv_tables.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.gv_tables.Location = new System.Drawing.Point(19, 140);
-            this.gv_tables.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gv_tables.Margin = new System.Windows.Forms.Padding(4);
             this.gv_tables.Name = "gv_tables";
             this.gv_tables.RowHeadersWidth = 51;
             this.gv_tables.Size = new System.Drawing.Size(587, 185);
@@ -60,16 +61,17 @@ namespace qlCSYT
             this.cb_user.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cb_user.FormattingEnabled = true;
             this.cb_user.Location = new System.Drawing.Point(78, 13);
-            this.cb_user.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cb_user.Margin = new System.Windows.Forms.Padding(4);
             this.cb_user.Name = "cb_user";
             this.cb_user.Size = new System.Drawing.Size(194, 31);
             this.cb_user.TabIndex = 1;
+            this.cb_user.SelectedIndexChanged += new System.EventHandler(this.cb_user_SelectedIndexChanged);
             // 
             // btn_addDelPriv
             // 
             this.btn_addDelPriv.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_addDelPriv.Location = new System.Drawing.Point(350, 13);
-            this.btn_addDelPriv.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_addDelPriv.Margin = new System.Windows.Forms.Padding(4);
             this.btn_addDelPriv.Name = "btn_addDelPriv";
             this.btn_addDelPriv.Size = new System.Drawing.Size(256, 47);
             this.btn_addDelPriv.TabIndex = 2;
@@ -80,7 +82,7 @@ namespace qlCSYT
             // 
             this.btn_addInsPriv.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_addInsPriv.Location = new System.Drawing.Point(350, 69);
-            this.btn_addInsPriv.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_addInsPriv.Margin = new System.Windows.Forms.Padding(4);
             this.btn_addInsPriv.Name = "btn_addInsPriv";
             this.btn_addInsPriv.Size = new System.Drawing.Size(256, 53);
             this.btn_addInsPriv.TabIndex = 3;
@@ -92,7 +94,7 @@ namespace qlCSYT
             this.cb_tblList.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cb_tblList.FormattingEnabled = true;
             this.cb_tblList.Location = new System.Drawing.Point(78, 91);
-            this.cb_tblList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cb_tblList.Margin = new System.Windows.Forms.Padding(4);
             this.cb_tblList.Name = "cb_tblList";
             this.cb_tblList.Size = new System.Drawing.Size(194, 31);
             this.cb_tblList.TabIndex = 4;
@@ -101,7 +103,7 @@ namespace qlCSYT
             // 
             this.rbtn_user.AutoSize = true;
             this.rbtn_user.Location = new System.Drawing.Point(280, 22);
-            this.rbtn_user.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbtn_user.Margin = new System.Windows.Forms.Padding(4);
             this.rbtn_user.Name = "rbtn_user";
             this.rbtn_user.Size = new System.Drawing.Size(17, 16);
             this.rbtn_user.TabIndex = 5;
@@ -113,7 +115,7 @@ namespace qlCSYT
             // 
             this.rbtn_role.AutoSize = true;
             this.rbtn_role.Location = new System.Drawing.Point(280, 61);
-            this.rbtn_role.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbtn_role.Margin = new System.Windows.Forms.Padding(4);
             this.rbtn_role.Name = "rbtn_role";
             this.rbtn_role.Size = new System.Drawing.Size(17, 16);
             this.rbtn_role.TabIndex = 7;
@@ -126,7 +128,7 @@ namespace qlCSYT
             this.cb_roles.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cb_roles.FormattingEnabled = true;
             this.cb_roles.Location = new System.Drawing.Point(78, 52);
-            this.cb_roles.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cb_roles.Margin = new System.Windows.Forms.Padding(4);
             this.cb_roles.Name = "cb_roles";
             this.cb_roles.Size = new System.Drawing.Size(194, 31);
             this.cb_roles.TabIndex = 6;
@@ -162,11 +164,22 @@ namespace qlCSYT
             this.lb_Table.TabIndex = 10;
             this.lb_Table.Text = "Table";
             // 
+            // GrantOpt_btn
+            // 
+            this.GrantOpt_btn.AutoSize = true;
+            this.GrantOpt_btn.Location = new System.Drawing.Point(33, 342);
+            this.GrantOpt_btn.Name = "GrantOpt_btn";
+            this.GrantOpt_btn.Size = new System.Drawing.Size(262, 21);
+            this.GrantOpt_btn.TabIndex = 11;
+            this.GrantOpt_btn.Text = "With grant option (for select, update)";
+            this.GrantOpt_btn.UseVisualStyleBackColor = true;
+            // 
             // frm_priviledges
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(633, 356);
+            this.ClientSize = new System.Drawing.Size(750, 383);
+            this.Controls.Add(this.GrantOpt_btn);
             this.Controls.Add(this.lb_Table);
             this.Controls.Add(this.lb_Role);
             this.Controls.Add(this.lb_User);
@@ -179,7 +192,7 @@ namespace qlCSYT
             this.Controls.Add(this.cb_user);
             this.Controls.Add(this.gv_tables);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frm_priviledges";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Thêm quyền";
@@ -203,5 +216,6 @@ namespace qlCSYT
         private System.Windows.Forms.Label lb_User;
         private System.Windows.Forms.Label lb_Role;
         private System.Windows.Forms.Label lb_Table;
+        private System.Windows.Forms.CheckBox GrantOpt_btn;
     }
 }
