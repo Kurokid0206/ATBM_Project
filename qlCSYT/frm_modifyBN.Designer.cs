66d30899f3469e93683942d7d0c9f002dfd134ad
trantinvan21087 <74363485+trantinvan21087@users.noreply.github.com>
2022-06-28 22:13:34 +0700

    update modifyBN

---
+++ qlCSYT/frm_modifyBN.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace qlCSYT
+{
+    partial class frm_modifyBN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_MaBN = new System.Windows.Forms.TextBox();
+            this.lb_MaBN = new System.Windows.Forms.Label();
+            this.cbMedFac = new System.Windows.Forms.ComboBox();
+            this.lb_csytID = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_MaBN
+            // 
+            this.tb_MaBN.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_MaBN.Location = new System.Drawing.Point(148, 6);
+            this.tb_MaBN.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_MaBN.Name = "tb_MaBN";
+            this.tb_MaBN.Size = new System.Drawing.Size(353, 31);
+            this.tb_MaBN.TabIndex = 4;
+            // 
+            // lb_MaBN
+            // 
+            this.lb_MaBN.AutoSize = true;
+            this.lb_MaBN.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_MaBN.Location = new System.Drawing.Point(4, 9);
+            this.lb_MaBN.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_MaBN.Name = "lb_MaBN";
+            this.lb_MaBN.Size = new System.Drawing.Size(130, 23);
+            this.lb_MaBN.TabIndex = 3;
+            this.lb_MaBN.Text = "Mã bệnh nhân";
+            // 
+            // cbMedFac
+            // 
+            this.cbMedFac.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMedFac.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbMedFac.FormattingEnabled = true;
+            this.cbMedFac.Location = new System.Drawing.Point(148, 45);
+            this.cbMedFac.Margin = new System.Windows.Forms.Padding(4);
+            this.cbMedFac.Name = "cbMedFac";
+            this.cbMedFac.Size = new System.Drawing.Size(354, 31);
+            this.cbMedFac.TabIndex = 36;
+            // 
+            // lb_csytID
+            // 
+            this.lb_csytID.AutoSize = true;
+            this.lb_csytID.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_csytID.Location = new System.Drawing.Point(4, 45);
+            this.lb_csytID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_csytID.Name = "lb_csytID";
+            this.lb_csytID.Size = new System.Drawing.Size(89, 23);
+            this.lb_csytID.TabIndex = 35;
+            this.lb_csytID.Text = "Mã CSYT:";
+            // 
+            // frm_modifyBN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 450);
+            this.Controls.Add(this.cbMedFac);
+            this.Controls.Add(this.lb_csytID);
+            this.Controls.Add(this.tb_MaBN);
+            this.Controls.Add(this.lb_MaBN);
+            this.Name = "frm_modifyBN";
+            this.Text = "Chỉnh sửa thông tin bệnh nhân";
+            this.Load += new System.EventHandler(this.frm_modifyBN_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_MaBN;
+        private System.Windows.Forms.Label lb_MaBN;
+        private System.Windows.Forms.ComboBox cbMedFac;
+        private System.Windows.Forms.Label lb_csytID;
+    }
+}
---
+++ qlCSYT/frm_modifyBN.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using qlCSYT.SqlConn;
+
+namespace qlCSYT
+{
+    public partial class frm_modifyBN : Form
+    {
+        public frm_modifyBN()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void frm_modifyBN_Load(object sender, EventArgs e)
+        {
+
+        }
+        //private void LoadBN()
+        //{
+        //    OracleConnection conn = DBUtils.GetDBConnection();
+        //    try
+        //    {
+        //        conn.Open();
+        //        OracleCommand cmd = new OracleCommand();
+        //        cmd.Connection = conn;
+        //        cmd.CommandText = "Select* from CSYT_ADMIN.VIEW_NHANVIEN_SELFVIEW";
+        //        using (DbDataReader reader = cmd.ExecuteReader())
+        //        {
+        //            if (reader.HasRows)
+        //            {
+        //                while (reader.Read())
+        //                {
+        //                    int MaBN_index = reader.GetOrdinal("MaBN");
+        //                    string MaBN = reader.GetString(1);
+
+        //                    int MaCSYT_index = reader.GetOrdinal("MaCSYT");
+        //                    int TenBN_index = reader.GetOrdinal("TenBN");
+        //                    int CMND_index = reader.GetOrdinal("CMND");
+        //                    int NgaySinh_index = reader.GetOrdinal("NgaySinh");
+        //                    int SoNha_index = reader.GetOrdinal("SoNha");
+        //                    int TenDuong_index = reader.GetOrdinal("TenDuong");
+        //                    int QuanHuyen_index = reader.GetOrdinal("QuanHuyen");
+        //                    int TinhTP_index = reader.GetOrdinal("TinhTP");
+        //                    int TienSuBenh_index = reader.GetOrdinal("TienSuBenh");
+        //                    int TienSuBenhGD_index = reader.GetOrdinal("TienSuBenhGD");
+        //                    int DiUngThuoc_index = reader.GetOrdinal("DiUngThuoc");
+        //                    int Username_index = reader.GetOrdinal("MaBN");
+
+        //                    string MaCSYT= reader.GetString(MaCSYT_index);
+        //                    string TenBN = reader.GetString(TenBN_index);
+        //                    string CMND = reader.GetString(CMND_index);
+        //                    DateTime NgaySinh = reader.GetDateTime(NgaySinh_index);
+        //                    string SoNha = reader.GetString(SoNha_index);
+        //                    string TenDuong = reader.GetString(TenDuong_index);
+        //                    string QuanHuyen = reader.GetString(QuanHuyen_index);
+        //                    string TinhTP = reader.GetString(TinhTP_index);
+        //                    string TienSuBenh = reader.GetString(TienSuBenh_index);
+        //                    string TienSuBenhGD = reader.GetString(TienSuBenhGD_index);
+        //                    string DiUngThuoc = reader.GetString(DiUngThuoc_index);
+        //                    string Username = reader.GetString(Username_index);
+        //                    Console.WriteLine(MaBN);
+        //                    TextBox Mytextbox = new TextBox();
+        //                    Mytextbox.Text = MaBN;
+        //                    this.Controls.Add(Mytextbox);
+
+
+        //                    //int empNameIndex = reader.GetOrdinal("Emp_Name");// 2
+        //                    //string empName = reader.GetString(empNameIndex);
+        //                    //int  userindex= reader.GetOrdinal("user");
+        //                    //string id = reader.GetString(userindex);
+        //                    //id = reader.GetString(0);
+        //                }
+
+        //            }
+        //        }
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine("Error: " + e);
+        //        Console.WriteLine(e.StackTrace);
+        //    }
+        //    finally
+        //    {
+        //        conn.Close();
+        //        conn.Dispose();
+        //    }
+        //    Console.Read();
+        //}
+    }
+}
